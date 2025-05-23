a27a390e43b3409651dd2aacda88e8b5d26cde1a
DIMAKOZHEMYAKA <dima.malevanyy.00@gmail.com>
2025-05-23 03:00:53 +0300

    Created Momento.java

---
+++ decorator/src/main/java/Models/Shapes/Momento.java
@@ -0,0 +1,36 @@
+package Models.Shapes;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class Momento {
+
+    private Queue<Object> momentoList = new ArrayDeque<>();
+
+
+    public void push(Object state) {
+        momentoList.add(state);
+    }
+
+    public Object poll() {
+        return momentoList.poll();
+    }
+
+    public boolean remove(Object state) {
+        return momentoList.remove(state);
+    }
+
+    public int getSize() {
+        return momentoList.size();
+    }
+
+    public ArrayList<Object> getListShapes() {
+        ArrayList<Object> objects = new ArrayList<>();
+        for (Object item: momentoList){
+            objects.add(item);
+        }
+        return objects;
+    }
+
+}
